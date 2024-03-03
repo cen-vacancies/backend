8f1310271801a4e572588615d9bb2ec7e13b2698
Евгений Рыбин <Evgeny.Rybin@urfu.me>
2024-03-03 19:19:50 +0500

    [#1] Инициализировать проект

---
+++ config/test.exs
@@ -0,0 +1,33 @@
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :cen, Cen.Repo,
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "cen_test#{System.get_env("MIX_TEST_PARTITION")}",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: System.schedulers_online() * 2
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :cen, CenWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "4aUyGJMb0C7QUBvsov/H3zTaPNMPNvm6lx6+3NahZTi8ajcyD/Ix3kXbprvS3AF9",
+  server: false
+
+# In test we don't send emails.
+config :cen, Cen.Mailer, adapter: Swoosh.Adapters.Test
+
+# Disable swoosh api client as it is only required for production adapters.
+config :swoosh, :api_client, false
+
+# Print only warnings and errors during test
+config :logger, level: :warning
+
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
