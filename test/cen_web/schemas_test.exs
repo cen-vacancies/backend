90ac3c5a27922af43afc0065d75690c903e1b3d9
Евгений Рыбин <Evgeny.Rybin@urfu.me>
2024-04-19 01:18:00 +0500

    Introduce tests ensuring API schema alignment with database schema

--- test/cen_web/schemas_test.exs
+++ test/cen_web/schemas_test.exs
@@ -3,6 +3,42 @@ defmodule CenWeb.SchemasTest do
 
   import CenWeb.Validator
 
+  alias CenWeb.Schemas
+
+  api_schemas_and_db_schemas = [
+    # API Schema - Ecto Schema - ignored fields
+    {Schemas.User, Cen.Accounts.User, ~w[hashed_password confirmed_at inserted_at updated_at]a},
+    {Schemas.Organization, Cen.Employers.Organization, ~w[employer_id inserted_at updated_at]a},
+    {Schemas.Vacancy, Cen.Employers.Vacancy, ~w[organization_id inserted_at updated_at]a}
+  ]
+
+  for {api_schema, db_schema, ignored_fields} <- api_schemas_and_db_schemas do
+    api_schema_name =
+      api_schema
+      |> Module.split()
+      |> Enum.join(".")
+
+    db_schema_name =
+      db_schema
+      |> Module.split()
+      |> Enum.join(".")
+
+    test "#{api_schema_name} matches #{db_schema_name}" do
+      unquote(api_schema)
+
+      api_fields =
+        unquote(api_schema).schema()
+        |> Map.fetch!(:properties)
+        |> Map.keys()
+
+      db_fields = unquote(db_schema).__schema__(:fields) -- unquote(ignored_fields)
+
+      fields_diff = db_fields -- api_fields
+
+      assert fields_diff == []
+    end
+  end
+
   with {:ok, modules} <- :application.get_key(:cen, :modules) do
     for module <- modules, match?(["CenWeb", "Schemas" | _rest], Module.split(module)) do
       ["CenWeb", "Schemas" | shortname_list] = Module.split(module)
