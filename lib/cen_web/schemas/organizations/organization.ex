9e125594e37fc4deccd10cbb34254a4cc955962e
Евгений Рыбин <Evgeny.Rybin@urfu.me>
2024-04-21 15:34:26 +0500

    Remove redundant schema modifying

--- lib/cen_web/schemas/organizations/organization.ex
+++ lib/cen_web/schemas/organizations/organization.ex
@@ -4,13 +4,6 @@ defmodule CenWeb.Schemas.Organization do
 
   require CenWeb.StrictAPISchema
 
-  user_emplyer_schema =
-    User.schema()
-    |> update_in([Access.key!(:properties), :role], fn _role_schema ->
-      %OpenApiSpex.Schema{type: :string, format: "employer"}
-    end)
-    |> update_in([Access.key!(:example), "role"], fn _role -> "employer" end)
-
   CenWeb.StrictAPISchema.schema(%{
     type: :object,
     properties: %{
@@ -20,7 +13,7 @@ defmodule CenWeb.Schemas.Organization do
       description: %{type: :string},
       address: %{type: :string},
       contacts: %{type: :string},
-      employer: user_emplyer_schema
+      employer: User.schema()
     },
     example: %{
       "id" => "756",
@@ -29,7 +22,7 @@ defmodule CenWeb.Schemas.Organization do
       "description" => "applicant",
       "address" => "620002, Свердловская область, г. Екатеринбург, ул. Мира, д. 19",
       "contacts" => "+78005553535",
-      "employer" => user_emplyer_schema.example
+      "employer" => User.schema().example
     }
   })
 end
