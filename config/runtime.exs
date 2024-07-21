a9715cb70c86a030ee171f696ee5f87702d3cb65
Евгений Рыбин <Evgeny.Rybin@urfu.me>
2024-07-22 03:03:44 +0500

    Disable check_origin

--- config/runtime.exs
+++ config/runtime.exs
@@ -72,7 +72,8 @@ if config_env() == :prod do
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
     ],
-    secret_key_base: secret_key_base
+    secret_key_base: secret_key_base,
+    check_origin: false
 
   # Configure Guardian
   config :cen, Cen.Token, secret_key: secret_key_base
